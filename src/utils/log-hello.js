be1d54423641b9672518ed1fb30e64e9e6f270c9
Jacob Ebey <jacob.ebey@live.com>
2021-01-13 21:41:22 -0800

    initial commit

---
+++ src/utils/log-hello.js
@@ -0,0 +1,7 @@
+function logHello(name) {
+  const sayHelloTo = name || "World";
+
+  console.log(`Hello, ${sayHelloTo}!`);
+}
+
+export default logHello;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+
+const { camelCase } = require("camel-case");
+
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+
+const pkg = require("./package.json");
+
+const name = camelCase(pkg.name);
+
+const exposes = {
+  "./utils/log-hello": "./src/utils/log-hello.js",
+};
+
+/** @type {webpack.Configuration} */
+const baseConfig = {
+  mode: process.env.NODE_ENV === "development" ? "development" : "production",
+};
+
+/** @type {webpack.Configuration} */
+const browserConfig = {
+  output: {
+    path: path.resolve("./dist/browser"),
+  },
+  plugins: [
+    new webpack.container.ModuleFederationPlugin({
+      name,
+      filename: "remote-entry.js",
+      exposes,
+    }),
+  ],
+};
+
+/** @type {webpack.Configuration} */
+const nodeConfig = {
+  target: "node",
+  output: {
+    path: path.resolve("./dist/node"),
+  },
+  plugins: [
+    new webpack.container.ModuleFederationPlugin({
+      name,
+      filename: "remote-entry.js",
+      library: { type: "commonjs" },
+      exposes,
+    }),
+  ],
+};
+
+module.exports = [
+  merge(baseConfig, browserConfig),
+  merge(baseConfig, nodeConfig),
+];
